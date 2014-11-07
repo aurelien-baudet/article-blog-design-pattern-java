9ce6ddbe5e273fc36677e5b15f6ba449ba0eb522
aurelien <aurelien@aurelien-lubuntubase>
2014-11-07 14:38:01 +0100

    début mise en place notifications

--- storage/src/main/java/fr/sii/storage/FileUtil.java
+++ storage/src/main/java/fr/sii/storage/FileUtil.java
@@ -9,73 +9,88 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Locale;
+import java.util.Properties;
 
-import com.dropbox.core.DbxAppInfo;
-import com.dropbox.core.DbxAuthFinish;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
 import com.dropbox.core.DbxClient;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.DbxWebAuthNoRedirect;
 import com.dropbox.core.DbxWriteMode;
 
 public class FileUtil {
 	private static DbxClient client;
+	
+	private static Twitter twitter;
 
-	public static void initDropbox(String dropboxAppKey, String dropboxAppSecret) throws IOException, DbxException {
-		DbxAppInfo appInfo = new DbxAppInfo(dropboxAppKey, dropboxAppSecret);
-
+	public static void initDropbox(String accessToken) throws IOException, DbxException {
 		// initialisation dropbox
-		DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",
-				Locale.getDefault().toString());
-		DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
-
-		// authentification et autorisation
-		String authorizeUrl = webAuth.start();
-		System.out.println("1. Go to: " + authorizeUrl);
-		System.out.println("2. Click \"Allow\" (you might have to log in first)");
-		System.out.println("3. Copy the authorization code.");
-		String code = new BufferedReader(new InputStreamReader(System.in))
-				.readLine().trim();
-
-		// authentifié
-		DbxAuthFinish authFinish = webAuth.finish(code);
-		String accessToken = authFinish.accessToken;
+		DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
 
 		client = new DbxClient(config, accessToken);
 	}
 	
-	public static void store(String path, String content) throws IOException, DbxException {
+	public static void initTwitter(String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
+        TwitterFactory twitterFactory = new TwitterFactory();
+        twitter = twitterFactory.getInstance();
+        twitter.setOAuthConsumer(consumerKey, consumerSecret);
+        twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+	}
+	
+	public static void store(String path, String content, String email) throws IOException, DbxException, TwitterException {
 		File file = new File(path);
 		file.createNewFile();
 		// try-with-resource (Java 7) : ferme automatiquement le stream
-		try(BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(file))) {
+		try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(file))) {
 			stream.write(content.getBytes());
 			stream.flush();
-		} catch(Throwable e) {
-			// on passe  l'erreur pour stocker avec DropBox
+			// Get system properties
+			Properties properties = System.getProperties();
+			properties.setProperty("mail.smtp.host", "localhost");
+			Session session = Session.getDefaultInstance(properties);
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("test@sii.fr"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+			message.setSubject("File "+path+" sucessfully stored");
+			message.setText("File "+path+" sucessfully stored");
+			// Send message
+			Transport.send(message);
+		} catch (Throwable e) {
+			// on passe l'erreur pour stocker avec DropBox
 		}
 		try (ByteArrayInputStream inputStream = new ByteArrayInputStream(content.getBytes())) {
-			client.uploadFile("/"+path, DbxWriteMode.force(), content.length(), inputStream);
+			client.uploadFile("/" + path, DbxWriteMode.force(), content.length(), inputStream);
+	        StatusUpdate statusUpdate = new StatusUpdate("File available on DropBox "+path);
+	        twitter.updateStatus(statusUpdate);
 		}
 	}
-	
+
 	public static String read(String path) throws FileNotFoundException, IOException, DbxException {
 		File file = new File(path);
-		try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 			String line = reader.readLine();
 			StringBuilder sb = new StringBuilder();
-			while(line!=null) {
+			while (line != null) {
 				sb.append(line);
 				line = reader.readLine();
 			}
 			return sb.toString();
-		} catch(Throwable e) {
+		} catch (Throwable e) {
 			// on passe l'erreur pour tenter de récupérer le fichier via DropBox
 		}
 		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-			client.getFile("/"+path, null, outputStream);
+			client.getFile("/" + path, null, outputStream);
 			return outputStream.toString();
 		}
 	}
--- storage/src/main/java/fr/sii/storage/Main.java
+++ storage/src/main/java/fr/sii/storage/Main.java
@@ -2,13 +2,15 @@ package fr.sii.storage;
 
 import java.io.IOException;
 
+import twitter4j.TwitterException;
+
 import com.dropbox.core.DbxException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, DbxException {
-		FileUtil.initDropbox("kycwqj1fstcn3vg", "kzmiubavm3ttner");
-		FileUtil.store("target/test.txt", "hello world");
+	public static void main(String[] args) throws IOException, DbxException, TwitterException {
+		FileUtil.initDropbox("uWvt2xN2BiAAAAAAAAAABJi8rBWLOrRd2Xetfic7KUlzaYkhM6VJ0KOWldnHCljy");
+		FileUtil.store("target/test.txt", "hello world", "aurelien.baudet@gmail.com");
 		System.out.println(FileUtil.read("target/test.txt"));
 	}
 }
