80f898d8192264606bdc5a0bfd3427b8ed9ad20e
aurelien <aurelien@aurelien-lubuntubase>
2014-11-09 23:03:16 +0100

    vraie sélection

--- storage/src/main/java/fr/sii/storage/FactorySelector.java
+++ storage/src/main/java/fr/sii/storage/FactorySelector.java
@@ -1,11 +1,26 @@
 package fr.sii.storage;
 
+import fr.sii.storage.factory.DropboxTweetFactory;
+import fr.sii.storage.factory.FileFactory;
 import fr.sii.storage.factory.FileMailAndDropboxTweetFactory;
 import fr.sii.storage.factory.StorageFactory;
 
 public class FactorySelector {
+	public static enum Context {
+		PUBLIC,
+		PRIVATE,
+		PUBLIC_REDUNDANCY
+	}
 
-	public static StorageFactory getFactory(String context) {
-		return new FileMailAndDropboxTweetFactory();
+	public static StorageFactory getFactory(Context context) {
+		switch(context) {
+			case PRIVATE:
+				return new FileFactory();
+			case PUBLIC:
+				return new DropboxTweetFactory();
+			case PUBLIC_REDUNDANCY:
+			default:
+				return new FileMailAndDropboxTweetFactory();
+		}
 	}
 }
--- storage/src/main/java/fr/sii/storage/Main.java
+++ storage/src/main/java/fr/sii/storage/Main.java
@@ -1,12 +1,13 @@
 package fr.sii.storage;
 
+import fr.sii.storage.FactorySelector.Context;
 import fr.sii.storage.exception.StoreException;
 import fr.sii.storage.store.Storage;
 
 public class Main {
 
 	public static void main(String[] args) throws StoreException {
-		Storage storage = FactorySelector.getFactory("prod").create();
+		Storage storage = FactorySelector.getFactory(Context.PUBLIC_REDUNDANCY).create();
 		storage.store("target/test.txt", "hello world");
 		System.out.println(storage.read("target/test.txt"));
 	}
